a0c843bde8d1fce05cbda911c67ac488e265d560
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2022-05-14 13:10:55 -0500

    Navbar final implementada

--- meddit_wa/src/components/NavBar.vue
+++ meddit_wa/src/components/NavBar.vue
@@ -1,63 +1,46 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#"
-            >Home <span class="sr-only">(current)</span></a
-          >
+    <img src="../assets/logo.png" height="50" />
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Inicio
+            </button>
+          </router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
+          <router-link to="/crearCuenta">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Crear Cuenta
+            </button>
+          </router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+          <router-link to="/IniciarSesion">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Acceder
+            </button>
+          </router-link>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
+    </div>
+
+    <form class="form-inline my-2 my-lg-0 ml-auto">
         <input
           class="form-control mr-sm-2"
           type="search"
-          placeholder="Search"
-          aria-label="Search"
+          placeholder="Buscar"
+          aria-label="Buscar"
         />
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
+        <button
+          class="btn btn-outline-success my-2 my-sm-0 float-lg-right"
+          type="submit"
+        >
+          Buscar
         </button>
       </form>
-    </div>
   </nav>
 </template>
 
--- meddit_wa/src/views/TheHome.vue
+++ meddit_wa/src/views/TheHome.vue
@@ -1,37 +1,6 @@
 <template>
   <div class="container col-xl-12 col-xxl-8 py-3">
-    <div class="row align-items-center g-lg-5">
-      <div class="col-lg-7 text-center text-lg-start">
-        <div class="col-lg-10 fs-4 font">
-          <p class="f_tamano">
-            Dedún es una plataforma para relacionar personas según sus intereses
-            y su psicología. Está llamada a ser la herramienta por defecto para
-            conocer personas, sin importar cuáles sean los intereses que motiven
-            al usuario en su búsqueda de otros usuarios: negocios, hobbies,
-            sexo, aprendizaje, no importa: la esencia del negocio es la misma
-            sin importar cuáles sean los matices que tengan, la esencia es
-            <b>la mente</b>.
-          </p>
-        </div>
-      </div>
-      <div class="col-md-10 mx-auto col-lg-5 font">
-        <form class="p-4 p-md-5 border rounded-3 bg-light">
-          <router-link to="/IniciarSesion">
-            <button class="w-100 btn btn-sm btn-primary color" type="submit">
-              Iniciar Sesión
-            </button>
-          </router-link>
-          <router-view />
-
-          <hr class="my-4" />
-          <router-link to="/crearCuenta">
-            <button class="w-100 btn btn-sm btn-primary color" type="submit">
-              Crear cuenta
-            </button>
-          </router-link>
-        </form>
-      </div>
-    </div>
+    
   </div>
 </template>
 
