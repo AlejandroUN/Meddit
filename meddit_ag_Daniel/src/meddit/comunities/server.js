14406266f2c72452c55d1d72cc4fe36a470c91fc
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2022-05-13 23:05:53 -0500

    Primer despliegue de Vue

--- meddit_ag_Daniel/src/meddit/comunities/server.js
+++
@@ -1,3 +0,0 @@
-export const url = '172.17.0.4'
-export const port = '4000'
-export const entryPoint = 'comunity'
--- meddit_ag_Daniel/src/meddit/snh/server.js
+++
@@ -1,4 +0,0 @@
-export const url = '172.17.0.4'
-//export const url = '172.17.0.2'
-export const port = '8080'
-export const entryPoint = 'api'
