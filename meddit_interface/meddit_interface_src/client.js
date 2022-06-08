3ed1253ef827c67b5e62dfa3f61c140bea65aa5f
Juan David <judgonzalezmu@unal.edu.co>
2022-06-07 22:12:10 -0500

    SOAP Client Creation

--- meddit_interface/meddit_interface_src/client.js
+++ meddit_interface/meddit_interface_src/client.js
@@ -2,8 +2,9 @@
 import soap from 'soap';
 
 
-const url = 'https://meddit-interface-3fafemp3rq-uc.a.run.app/wsdl?wsdl';
+// const url = 'https://meddit-interface-3fafemp3rq-uc.a.run.app/wsdl?wsdl';
 
+const url = 'https://gamestack-soap-e3wbalmwuq-uc.a.run.app/service/videogamesWSDL.wsdl'
 
 
 // Create client
@@ -16,12 +17,12 @@ soap.createClient(url, function(err, client) {
      * in the WSDL file
      */
     var args = {
-        id: 1
+        // id: 1
     };
     // call the service
-    client.ComunityBId(args, function(err, res) {
+    client.Videogame(args, function(err, res) {
         if (err){
-            throw err;}
+            console.log(err.message);}
         // print the service returned result
         console.log(res);
     });
