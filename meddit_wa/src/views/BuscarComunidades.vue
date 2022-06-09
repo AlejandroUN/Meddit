61ad5169b163f192c33e33d19dedbb0b29921ba4
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2022-06-09 01:30:05 -0500

    Front con interface terminado

--- meddit_wa/src/views/BuscarComunidades.vue
+++ meddit_wa/src/views/BuscarComunidades.vue
@@ -1,55 +1,54 @@
 <template>
   <div id="app">
     <NavBarComunidades />
-  <div class="container col-xl-12 col-xxl-8 py-3">
-    <div class="row mt-1">
-            <table class="table table-boarded">
-                <thead>
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    <div v-for="category in allComunities" :key="category.id">
-                        <tr v-if="category.name==nametoFind">
-                        <td>{{category.id}}</td>
-                        <td>{{category.name}}</td>
-                        <td><router-link :to="'/Comunidad/'+category.id+'/Menu'"><button>Entrar</button></router-link></td>
-                        </tr>
-                    </div>
-                </tbody>
-            </table>
-        </div>
-  </div>
+    <div class="container col-xl-12 col-xxl-8 py-3">
+      <div class="row mt-1">
+        <table class="table table-boarded">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <div v-for="category in allComunities" :key="category.id">
+              <tr v-if="category.name == nametoFind">
+                <td>{{ category.id }}</td>
+                <td>{{ category.name }}</td>
+                <td>
+                  <router-link :to="'/Comunidad/' + category.id + '/Menu'"
+                    ><button>Entrar</button></router-link
+                  >
+                </td>
+              </tr>
+            </div>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-import NavBarComunidades from '../components/NavBarComunidades.vue'
-import gql from 'graphql-tag'
+import NavBarComunidades from "../components/NavBarComunidades.vue";
+import gql from "graphql-tag";
 export default {
-  name: 'App',
-  components: { NavBarComunidades},
+  name: "App",
+  components: { NavBarComunidades },
   apollo: {
-      
-      allComunities: gql`query {
-  allComunities{
-    id
-    name
-  }
-}`,
-    
-    },
-    data(){
-      return{
-        nametoFind:this.$route.params.name,
-        
+    allComunities: gql`
+      query {
+        allComunities {
+          id
+          name
+        }
       }
+    `,
   },
-    
-  
-}
+  data() {
+    return {
+      nametoFind: this.$route.params.name,
+    };
+  },
+};
 </script>
--- meddit_wa/src/views/LittleInterface.vue
+++ meddit_wa/src/views/LittleInterface.vue
@@ -14,43 +14,42 @@
               <div class="col-md-7 col-lg-6 offset-md-3">
                 <br />
                 <br />
-                <h4 class="mb-3">Videojuego</h4>
+                <h4 class="mb-3">Videojuegos</h4>
+                <button
+                  class="btn boton_MEDDIT2"
+                  type="button"
+                  v-on:click="pedir"
+                >
+                  Pedir
+                </button>
+                <!--{{ lista }}-->
+                <br />
+                <br />
 
-                <div class="input-group mb-3">
-                  <input
-                    type="number"
-                    class="form-control"
-                    placeholder="id"
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                    name="id"
-                    v-model="id"
-                    autocomplete="off"
-                  />
-                  <div class="input-group-append">
-                    <button class="btn boton_MEDDIT2" type="button">
-                      Go!
-                    </button>
+                <div class="container col-xl-12 col-xxl-8 py-3">
+                  <div class="row mt-1">
+                    <table class="table">
+                      <thead class="thead-dark">
+                        <tr>
+                          <th scope="col">Titulo</th>
+                          <th scope="col">Descripción</th>
+                          <th scope="col">Portada</th>
+                          <th scope="col">Puntuación</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="i in lista" :key="i.Id">
+                          
+                            <th scope="row">{{ i.title }}</th>
+                            <td>{{ i.description }}</td>
+                            <td><img v-bind:src="i.cover" height="100" /></td>
+                            <td>{{ i.score }}</td>
+                          
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
-                <table class="table">
-                  <thead class="thead-dark">
-                    <tr>
-                      <th scope="col">Titulo</th>
-                      <th scope="col">Descripción</th>
-                      <th scope="col">Portada</th>
-                      <th scope="col">Puntuación</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">{{titulo}}</th>
-                      <td>{{descripcion}}</td>
-                      <td><img v-bind:src="portada" height="100"></td>
-                      <td>{{puntuacion}}</td>
-                    </tr>
-                  </tbody>
-                </table>
               </div>
             </div>
           </main>
@@ -62,17 +61,26 @@
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import axios from "axios";
 export default {
   name: "App",
   components: { NavBar },
   data() {
     return {
-      id: null,
-      titulo: "Halo Combat Envolved Primer Generación JAJ salu2",
-      descripcion: "Voy a escribir un montón de pendejadas aquí para ver como afecta el tamaño de la tabla el hacer esa mierda así, puta madre, que se acabe rápido esta mierda :cccccccc",
-      portada: 'https://mspoweruser.com/wp-content/uploads/2017/05/Halo1.jpg',
-      puntuacion: 3.2
+      lista: [],
     };
+  },
+  methods: {
+    pedir() {
+      axios
+        .get("https://meddit-interface-3fafemp3rq-uc.a.run.app/videoGames")
+        .then((response) => {
+          this.lista = response.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
 };
 </script>
